03583202bafb28c8df7e08a50c6256ad60d112eb
GoldenThumbs <skiffsam@gmail.com>
2020-11-25 01:54:34 -0600

    More misc AO changes, hooray!!!
    Mostly just adjusted AO calculation to be more similar to HBAO. Also took a few peaks at frictional's SSAO code and tried to integrate some ideas here.

--- bin/CoreData/Shaders/GLSL/SSAO.glsl
+++ bin/CoreData/Shaders/GLSL/SSAO.glsl
@@ -14,22 +14,22 @@ void VS()
     vec3 worldPos = GetWorldPos(modelMatrix);
     gl_Position = GetClipPos(worldPos);
     vTexCoord = GetQuadTexCoord(gl_Position);
-    vViewRay = vec3(
-        gl_Position.x / gl_Position.w * cFrustumSize.x,
-        gl_Position.y / gl_Position.w * cFrustumSize.y,
-        cFrustumSize.z);
+    vViewRay = gl_Position.xyz;
 }
 
 #ifdef COMPILEPS
     uniform sampler2D sRnd0; // Random noise
-    uniform sampler2D sDep1; // Depth
+    uniform sampler2D sDep1; // Depth (also sometimes position, fuck you)
 
-    float GetDepth(vec2 uv)
+    // VVV---Usless texture sampler---VVV
+    //uniform sampler2D sDep2; // Full-resolution depth (for depth-aware upsampling/blur)
+
+    float GetDepth(sampler2D depthSampler, vec2 uv)
     {
         #ifdef HWDEPTH
-            return ReconstructDepth(texture2D(sDep1, uv).r);
+            return ReconstructDepth(texture2D(depthSampler, uv).r);
         #else
-            return DecodeDepth(texture2D(sDep1, uv).rgb);
+            return DecodeDepth(texture2D(depthSampler, uv).rgb);
         #endif
     }
 
@@ -37,12 +37,12 @@ void VS()
         uniform vec2 cSSAOInvSize;
 
         uniform float cRadius;
-        uniform float cScale;
         uniform float cBias;
         uniform float cStrength;
         uniform vec2 cNoiseScale;
 
         const int samples = 8;
+        const float steps = 3.0;
         const vec2 kernel[samples] = vec2[](
             vec2( 1, 0), vec2(-1, 0),
             vec2( 0, 1), vec2( 0,-1),
@@ -56,6 +56,14 @@ void VS()
             return p;
         }
 
+        vec3 MinDiff(vec3 P, vec3 Pr, vec3 Pl)
+        {
+          vec3 V1 = Pr - P;
+          vec3 V2 = P - Pl;
+          return (dot(V1,V1) < dot(V2,V2)) ? V1 : V2;
+        }
+
+        // requires 32 bit floating point precision on position buffer to look ok
         vec3 GetNrm(vec3 p0)
         {
             // Normal generation based on: https://github.com/GameTechDev/ASSAO/blob/master/Projects/ASSAO/ASSAO/ASSAO.hlsl#L279
@@ -84,19 +92,64 @@ void VS()
             return -normalize(normal);
         }
 
-        float GetAO(vec2 offset, vec3 p, vec3 n)
+        //vec3 GetNrm(vec3 p0)
+        //{
+
+        //    vec3 p1X = GetPos(vTexCoord+vec2(cGBufferInvSize.x, 0.0));
+        //    vec3 p2X = GetPos(vTexCoord-vec2(cGBufferInvSize.x, 0.0));
+
+        //    vec3 p1Y = GetPos(vTexCoord+vec2(0.0, cGBufferInvSize.y));
+        //    vec3 p2Y = GetPos(vTexCoord-vec2(0.0, cGBufferInvSize.y));
+
+        //    return -normalize(cross(MinDiff(p0, p1X, p2X), MinDiff(p0, p1Y, p2Y)));
+        //}
+
+        float GetAO(vec2 offset, vec3 p, vec3 n, float r)
         {
-            vec3 diff = GetPos(vTexCoord + offset) - p;
-            vec3 v = normalize(diff);
-            return max(dot(n, v) - cBias, 0.0) * smoothstep(0.0, 1.0, cRadius / abs(diff.z * cScale));
+            vec2 offset0 = r * offset;
+            vec2 offset1 = vec2(offset0.x*0.707 - offset0.y*0.707, offset0.x*0.707 + offset0.y*0.707);
+
+            // ao calculation based on: https://frictionalgames.com/2014-01-tech-feature-ssao-and-temporal-blur/
+            // and also: https://www.gamedev.net/articles/programming/graphics/a-simple-and-practical-approach-to-ssao-r2753/
+
+            vec3 diff0 = GetPos(vTexCoord + offset0*0.75) - p;
+            vec3 diff1 = GetPos(vTexCoord + offset1*0.50) - p;
+            vec3 diff2 = GetPos(vTexCoord + offset0*0.25) - p;
+            vec3 diff3 = GetPos(vTexCoord + offset1) - p;
+
+            vec4 distSqr = vec4(dot(diff0, diff0),
+                                dot(diff1, diff1),
+                                dot(diff2, diff2),
+                                dot(diff3, diff3));
+            
+            vec4 invLength = inversesqrt(distSqr);
+
+            vec4 falloff = clamp(1.0 + distSqr * invLength * -(1.0-r), vec4(0.0), vec4(1.0));
+
+            vec4 angle = vec4(dot(n, diff0),
+                              dot(n, diff1),
+                              dot(n, diff2),
+                              dot(n, diff3)) * invLength;
+
+            return dot(max(angle - cBias, vec4(0.0)), falloff) * 0.25;
         }
     #elif defined(GEN_BLUR)
-        float BoxBlur(vec2 offset, float baseD)
+        const float KERNEL_RADIUS = 3;
+        uniform vec2 cBlurDir;
+
+        float BoxBlur(vec2 offset, float baseD, float r, inout float w_total)
         {
             float ao = texture2D(sDiffMap, vTexCoord + offset).r;
-            float d = GetDepth(vTexCoord + offset);
-            float ddiff = (baseD - d) * 50.0;
-            return ao * exp2(ddiff * ddiff);
+            float d = GetDepth(sDep1, vTexCoord + offset);
+
+            const float BlurSigma = float(KERNEL_RADIUS) * 0.5;
+            const float BlurFalloff = 1.0 / (2.0*BlurSigma*BlurSigma);
+
+            float ddiff = abs(d - baseD) * 50;
+            float w = exp2(-r*r*BlurFalloff * ddiff*ddiff);
+            w_total += w;
+
+            return ao*w;
         }
     #endif
 
@@ -104,24 +157,36 @@ void VS()
 
 void PS()
 {   
-    #if defined(GEN_POS)
-        vec2 uv = gl_FragCoord.xy * cGBufferInvSize;
-        gl_FragColor.rgb = vViewRay * GetDepth(uv);
+    #if defined(DOWNRES_DEPTH)
+        vec4 d = texelFetch(sDep1, ivec2(gl_FragCoord.xy * 2.0), 0);
+        vec4 d0 = texelFetch(sDep1, ivec2(gl_FragCoord.xy * 2.0) + ivec2( 1, 0.0), 0);
+        vec4 d1 = texelFetch(sDep1, ivec2(gl_FragCoord.xy * 2.0) + ivec2(-1, 0.0), 0);
+        vec4 d2 = texelFetch(sDep1, ivec2(gl_FragCoord.xy * 2.0) + ivec2( 0.0, 1), 0);
+        vec4 d3 = texelFetch(sDep1, ivec2(gl_FragCoord.xy * 2.0) + ivec2( 0.0,-1), 0);
+        gl_FragColor = max(d, max(max(d0, d1), max(d2, d3)));
+    #elif defined(GEN_POS)
+        vec3 p = vViewRay * GetDepth(sDep1, vTexCoord);
+        gl_FragColor.rgb = p;
     #elif defined(GEN_AO)
         vec3 pos = GetPos(vTexCoord);
         vec3 nrm = GetNrm(pos);
         vec2 rnd = texture2D(sRnd0, vTexCoord / (cNoiseScale * cGBufferInvSize)).xy * 2.0 - 1.0;
 
-        float rad = cRadius / pos.z;
+        float radStep = (cRadius / pos.z) / float(steps + 1);
         float ao = 0.0;
         for (lowp int i=0; i<samples; i++)
         {
-            vec2 coord = rad * reflect(kernel[i], rnd);
-            vec2 coord2 = vec2(coord.x*0.707 - coord.y*0.707, coord.x*0.707 + coord.y*0.707);
-            ao += GetAO(coord*0.25, pos, nrm) * 0.25;
-            ao += GetAO(coord*0.50, pos, nrm) * 0.25;
-            ao += GetAO(coord*0.75, pos, nrm) * 0.25;
-            ao += GetAO(coord, pos, nrm) * 0.25;
+            float traceResult = 0.0;
+            vec2 coord = reflect(kernel[i], rnd);
+
+            float rad = 0.0;
+            for (lowp float r=0; r<steps; ++r)
+            {
+                rad += radStep;
+                float occ = GetAO(coord, pos, nrm, rad);
+                traceResult = mix(occ, 1.0, traceResult);
+            }
+            ao += traceResult;
         }
 
         ao /= samples;
@@ -131,15 +196,22 @@ void PS()
         gl_FragColor.rgb = vec3(ao);
     #elif defined(GEN_BLUR)
         float ao = texture2D(sDiffMap, vTexCoord).r;
-        float depth = GetDepth(vTexCoord);
-        ao += BoxBlur( vec2(1, 0) * cGBufferInvSize, depth);
-        ao += BoxBlur(-vec2(1, 0) * cGBufferInvSize, depth);
-        ao += BoxBlur( vec2(0, 1) * cGBufferInvSize, depth);
-        ao += BoxBlur(-vec2(0, 1) * cGBufferInvSize, depth);
-        ao += BoxBlur( vec2(1, 1) * cGBufferInvSize, depth);
-        ao += BoxBlur(-vec2(1, 1) * cGBufferInvSize, depth);
-        ao += BoxBlur(vec2(-1, 1) * cGBufferInvSize, depth);
-        ao += BoxBlur(vec2( 1,-1) * cGBufferInvSize, depth);
-        gl_FragColor.rgb = vec3(ao/8.0);
+        float depth = GetDepth(sDep1, vTexCoord);
+
+        float w_total = 1.0;
+
+        for (float r = 1; r <= KERNEL_RADIUS; ++r)
+        {
+            vec2 offset = cBlurDir * cGBufferInvSize * r;
+            ao += BoxBlur(offset, r, depth, w_total);  
+        }
+
+        for (float r = 1; r <= KERNEL_RADIUS; ++r)
+        {
+            vec2 offset = cBlurDir * -cGBufferInvSize * r;
+            ao += BoxBlur(offset, r, depth, w_total);  
+        }
+
+        gl_FragColor.rgb = vec3(ao/w_total);
     #endif
 }
